bc047c49fac2393733bd6880950ec693772f8798
DSDmark <sahup3296@gmail.com>
2022-06-07 14:00:29 +0530

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,23 @@
+import {
+  Header,
+  Home,
+  About,
+  Service,
+  Portfolio,
+  Contact,
+} from "../components";
+
+const index = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+      <About />
+      <Service />
+      <Portfolio />
+      <Contact />
+    </>
+  );
+};
+
+export default index;
