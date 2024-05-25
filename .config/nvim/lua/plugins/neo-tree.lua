7c29d05caebb1009618fbf2e9c4295e606df8a5b
DSDmark <sahup3296@gmail.com>
2024-05-25 14:12:24 +0530

    update config V1.0.2

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,4 +1,4 @@
-return	{
+return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   dependencies = {
@@ -6,17 +6,24 @@ return	{
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
-  config=function()
-    require'neo-tree'.setup({window = {width = 25,
-      height = 15,
-      auto_expand_width = false,
-      filesystem = {filtered_items = {
-        hide_dotfiles = false
-      }},
-      mappings = {
-        ["l"] = "open"
-      }
-    }})
-  end
+  config = function()
+    require("neo-tree").setup({
+      window = {
+        width = 25,
+        height = 15,
+        auto_expand_width = false,
+        filesystem = {
+          filtered_items = {
+            visible = true,
+            show_hidden_count = true,
+            hide_dotfiles = true,
+            hide_gitignored = true,
+          },
+        },
+        mappings = {
+          ["l"] = "open",
+        },
+      },
+    })
+  end,
 }
-
